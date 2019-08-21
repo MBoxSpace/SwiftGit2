6896ff3859fe68b671dce588b05ddf8279a2ee4f
Whirlwind James <whirlwindjames@foxmail.com>
2019-08-21 22:18:30 +0800

    add clean support

---
+++ SwiftGit2/Options/CleanOptions.swift
@@ -0,0 +1,26 @@
+//
+//  CleanOptions.swift
+//  SwiftGit2
+//
+//  Created by Whirlwind on 2019/8/13.
+//  Copyright © 2019 GitHub, Inc. All rights reserved.
+//
+
+import Foundation
+import libgit2
+
+public struct CleanOptions: OptionSet {
+    public let rawValue: Int
+
+    public init(rawValue: Int) {
+        self.rawValue = rawValue
+    }
+
+    public static let force          = CleanOptions(rawValue: 1 << 0)
+    public static let directory      = CleanOptions(rawValue: 1 << 1)
+    public static let includeIgnored = CleanOptions(rawValue: 1 << 2)
+    public static let onlyIgnored    = CleanOptions(rawValue: 1 << 3)
+
+    public static let dryRun         = CleanOptions(rawValue: 1 << 4)
+}
+
---
+++ SwiftGit2/Repository+Clean.swift
@@ -0,0 +1,35 @@
+//
+//  Repository+Clean.swift
+//  SwiftGit2
+//
+//  Created by Whirlwind on 2019/8/13.
+//  Copyright © 2019 GitHub, Inc. All rights reserved.
+//
+
+import Foundation
+import libgit2
+
+public extension Repository {
+    func clean(_ options: CleanOptions, shouldRemove: ((String) -> Bool)? = nil) -> Result<[String], NSError> {
+        return status().flatMap { entries -> Result<[String], NSError> in
+            let s = entries.filter({ entry -> Bool in
+                if entry.status == .workTreeNew && !options.contains(.onlyIgnored) {
+                    return true
+                } else if entry.status == .ignored && (options.contains(.includeIgnored) || options.contains(.onlyIgnored)) {
+                    return true
+                }
+                return false
+            }).compactMap { $0.indexToWorkDir?.newFile?.path }
+            if !options.contains(.dryRun) {
+                for path in s {
+                    if let block = shouldRemove, !block(path) {
+                        continue
+                    }
+                    guard let url = directoryURL?.appendingPathComponent(path) else { continue }
+                    try? FileManager.default.removeItem(at: url)
+                }
+            }
+            return .success(s)
+        }
+    }
+}
