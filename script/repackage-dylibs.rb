52bc4f7fed3cc7fe0b1e86a2675491e01c4ad99c
Whirlwind James <whirlwindjames@foxmail.com>
2019-08-30 15:46:49 +0800

    fix repackage not work

--- script/repackage-dylibs.rb
+++ script/repackage-dylibs.rb
@@ -15,7 +15,6 @@
 
 
 require 'fileutils'
-require 'ostruct'
 
 def err(msg)
   puts "error: " + msg
@@ -27,28 +26,41 @@ def warn(msg)
 end
 
 def note(msg)
-  puts "note: " + msg
+  puts msg
+end
+
+def exec(command)
+  note(command)
+  out = `#{command}`
+  err "command failed!" if $? != 0
+  out
 end
 
 envvars = %w(
   TARGET_BUILD_DIR
   EXECUTABLE_PATH
-  FRAMEWORKS_FOLDER_PATH
   LIBRARY_SEARCH_PATHS
+  FRAMEWORK_SEARCH_PATHS
+  FRAMEWORKS_FOLDER_PATH
   SRCROOT
+  INFOPLIST_PATH
 )
 
 envvars.each do |var|
-  raise "Must be run in an Xcode Run Phase" unless ENV[var]
-  Kernel.const_set var, ENV[var]
+  Kernel.const_set(var, ENV[var])
 end
 
+require 'shellwords'
 TARGET_EXECUTABLE_PATH = File.join(TARGET_BUILD_DIR, EXECUTABLE_PATH)
-TARGET_FRAMEWORKS_PATH = File.join(TARGET_BUILD_DIR, FRAMEWORKS_FOLDER_PATH)
-LIBRARY_SEARCH_PATH_ARRAY = LIBRARY_SEARCH_PATHS.split(" ")
+TARGET_FRAMEWORKS_PATH = FRAMEWORKS_FOLDER_PATH ? File.join(TARGET_BUILD_DIR, FRAMEWORKS_FOLDER_PATH) : TARGET_BUILD_DIR
+FRAMEWORK_SEARCH_PATH_ARRAY = Shellwords::shellwords(FRAMEWORK_SEARCH_PATHS)
+LIBRARY_SEARCH_PATH_ARRAY = Shellwords::shellwords(LIBRARY_SEARCH_PATHS)
 
 def search_library(name)
-  LIBRARY_SEARCH_PATH_ARRAY.each do |path|
+  name = name.sub("@rpath/", '')
+  is_framework = name =~ /\.framework\//
+  paths = is_framework ? FRAMEWORK_SEARCH_PATH_ARRAY : LIBRARY_SEARCH_PATH_ARRAY
+  paths.each do |path|
     path = File.join(SRCROOT, path) if File.absolute_path(path) != path
     path = File.join(path, name)
     return path if File.exists?(path)
@@ -56,8 +68,8 @@ def search_library(name)
   return nil
 end
 
-ALL_DEPS = []
 def extract_link_dependencies(executable)
+  puts "extract_link_dependencies: #{executable}"
   unless File.exist?(executable)
     warn "executable not exists: #{executable}"
     return []
@@ -66,82 +78,87 @@ def extract_link_dependencies(executable)
 
   lines = deps.split("\n").map(&:strip)
   lines.shift
-  # lines.shift
+  lines.shift
   lines.map do |dep|
     path, compat, current = /^(.*) \(compatibility version (.*), current version (.*)\)$/.match(dep)[1..3]
     err "Failed to parse #{dep}" if path.nil?
+    path = search_library(path) if path =~ /^@rpath\//
 
-    name = File.basename(path)
-    next if ALL_DEPS.include?(name)
-    ALL_DEPS << name
-
-    dep = OpenStruct.new
-    dep.is_self = (File.basename(path) == File.basename(executable))
-    dep.executable = executable
-    dep.install_name = path
-    dep.current_version = current
-    dep.compat_version = compat
-    dep.type = File.extname(path)
-    dep.name = name
-    dep.is_packaged = !!(dep.install_name =~ /^@rpath/)
-    dep.path = if dep.install_name =~ /^@rpath/
-      search_library(dep.name)
+    if path.nil? || path =~ /^(\/usr\/lib|\/System\/Library)/ 
+      nil
     else
-      dep.install_name
+      note "Found link: #{path}"
+      path
     end
-
-    dep.path.nil? ? nil : dep
   end.compact
 end
 
-def repackage_dependency(dep)
-  return if dep.is_self or dep.path =~ /^(\/usr\/lib|\/System\/Library)/
-
-  note "Packaging #{dep.name}…"
-
-  FileUtils.mkdir(TARGET_FRAMEWORKS_PATH) unless Dir.exist?(TARGET_FRAMEWORKS_PATH)
-  packaged_path = File.join(TARGET_FRAMEWORKS_PATH, dep.name)
-
-  case dep.type
-  when ".dylib"
-    if File.exist? packaged_path
-      note "#{dep.path} already in Frameworks directory, removing"
-      FileUtils.rm packaged_path
-    end
-
-    note "Copying #{dep[:path]} to #{TARGET_FRAMEWORKS_PATH}"
-    FileUtils.cp dep[:path], TARGET_FRAMEWORKS_PATH
-    FileUtils.chmod "u=rw", packaged_path
-
-    unless dep.is_packaged
-      out = `install_name_tool -change #{dep.path} "@rpath/#{dep.name}" #{dep.executable}`
-      if $? != 0
-        err "install_name_tool failed with error #{$?}:\n#{out}"
-      end
-
-      dep.path = packaged_path
-      dep.install_name = "@rpath/#{dep.name}"
-      dep.is_packaged = true      
-    end
+def link_name(path)
+  if path =~ /([^\/]+\.framework\/)(Versions\/[^\/]*\/)([^\/]*)$/
+    "#{$1}#{$3}"
+  elsif path =~ /([^\/]+\.framework\/)([^\/]*)$/
+    "#{$1}#{$2}"
+  elsif path =~ /([^\/]*)$/
+    $1
   else
-    warn "Unhandled type #{dep.type} for #{dep.path}, ignoring"
+    path
   end
 end
 
-def fix_install_id(dep)
-  note "Fixing #{dep.name} install_name id…"
-  out = `install_name_tool -id @rpath/#{dep.name} #{dep.executable}`
-  if $? != 0
-    err "install_name_tool failed with error #{$?}:\n#{out}"
+def change_link_path(executable, path)
+  name = link_name(path)
+  exec("install_name_tool -change #{path} @rpath/#{name} '#{executable}'")
+  name
+end
+
+def fix_install_id(executable)
+  name = link_name(executable)    
+  exec("install_name_tool -id @rpath/#{name} '#{executable}'")
+  name
+end
+
+def copy_dylib(path)
+  path = path.sub(/\.framework\/.*/, ".framework")
+  packaged_path = File.join(TARGET_FRAMEWORKS_PATH, File.basename(path))
+
+  if File.exist? packaged_path
+    note "#{packaged_path} already in Frameworks directory, removing"
+    FileUtils.rm_rf packaged_path
+  end
+
+  note "Copying #{path} to #{TARGET_FRAMEWORKS_PATH}"
+  FileUtils.mkdir(TARGET_FRAMEWORKS_PATH) unless Dir.exist?(TARGET_FRAMEWORKS_PATH)
+  FileUtils.cp_r path, TARGET_FRAMEWORKS_PATH
+  FileUtils.chmod "u=rw", packaged_path
+  packaged_path
+end
+
+paths = [TARGET_EXECUTABLE_PATH]
+
+
+ALL_DEPS = []
+def parse(executable)
+  paths = extract_link_dependencies(executable)
+  paths.select! do |path|
+    name = change_link_path(executable, path)
+    if ALL_DEPS.include?(File.basename(executable))
+      false
+    else
+      ALL_DEPS << File.basename(executable)
+      true
+    end
+  end
+
+  paths.each do |path|
+    note "================ #{File.basename(path)} ================"
+    path = copy_dylib(path)
+    fix_install_id(path)
+    parse(path)
   end
 end
 
-deps = extract_link_dependencies(TARGET_EXECUTABLE_PATH)
-while (dep = deps.shift) do
-  repackage_dependency dep
-  fix_install_id dep if dep.is_self and dep.executable != TARGET_EXECUTABLE_PATH
-  deps += extract_link_dependencies(dep[:path]) if dep.is_packaged and not dep.is_self and dep.path
-end
+note "================ #{File.basename(TARGET_EXECUTABLE_PATH)} ================"
+parse(TARGET_EXECUTABLE_PATH)
 
 note "Packaging done"
 exit 0
