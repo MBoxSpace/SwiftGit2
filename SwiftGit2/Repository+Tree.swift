1e7bfcda5048037b96f99df79f7cbdaefc71d693
Whirlwind James <whirlwindjames@foxmail.com>
2020-06-03 11:43:56 +0800

    Add ahead/behind api

--- SwiftGit2/Repository+Tree.swift
+++ SwiftGit2/Repository+Tree.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+import git2
+
+public extension Repository {
+    func aheadBehind(local: OID, upstream: OID) -> Result<(ahead: size_t, behind: size_t), NSError> {
+        var ahead: size_t = 0
+        var behind: size_t = 0
+        var localOID = local.oid
+        var upstreamOID = upstream.oid
+        let result = git_graph_ahead_behind(&ahead, &behind, self.pointer, &localOID, &upstreamOID)
+        guard result == GIT_OK.rawValue else {
+            return .failure(NSError(gitError: result, pointOfFailure: "git_graph_ahead_behind"))
+        }
+        return .success((ahead: ahead, behind: behind))
+    }
+
+    func aheadBehind(local: Branch, upstream: Branch) -> Result<(ahead: size_t, behind: size_t), NSError> {
+        return aheadBehind(local: local.oid, upstream: upstream.oid)
+    }
+}
